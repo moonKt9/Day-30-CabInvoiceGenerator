676bff4b34e347192111822ca63bbb5f01991c28
Kritika <kritistarsmind@gmail.com>
2022-09-02 23:49:23 +0530

    cabinvoicegenerator

---
+++ src/com/invoice/test/CabInvoiceGenerator.java
@@ -0,0 +1,5 @@
+package com.invoice.test;
+
+public class CabInvoiceGenerator {
+
+}
---
+++ src/com/invoice/test/CabInvoiceGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.invoice.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
